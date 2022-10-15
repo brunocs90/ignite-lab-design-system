9ffea41392437534721409d3704eff7a5cd4f9e9
brunocs90@gmail.com <brunocs90@gmail.com>
2022-10-15 16:13:01 -0300

    test app

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -4,11 +4,11 @@ import { Check } from 'phosphor-react'
 export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps { }
 
 export function Checkbox(props: CheckboxProps) {
-    return (
-        <CheckboxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded" {...props}>
-            <CheckboxPrimitive.Indicator asChild>
-                <Check weight="bold" className="h-5 w-5 text-cyan-500" />
-            </CheckboxPrimitive.Indicator>
-        </CheckboxPrimitive.Root>
-    )
+	return (
+		<CheckboxPrimitive.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded" {...props}>
+			<CheckboxPrimitive.Indicator asChild>
+				<Check weight="bold" className="h-5 w-5 text-cyan-500" />
+			</CheckboxPrimitive.Indicator>
+		</CheckboxPrimitive.Root>
+	)
 }
--- src/components/TextInput.stories.tsx
+++ src/components/TextInput.stories.tsx
@@ -3,29 +3,29 @@ import { Envelope } from 'phosphor-react'
 import { TextInput, TextInputRootProps } from './TextInput'
 
 export default {
-  title: 'Components/TextInput',
-  component: TextInput.Root,
-  args: {
-    children: [
-      <TextInput.Icon>
-        <Envelope />
-      </TextInput.Icon>,
-      <TextInput.Input placeholder="Type your e-mail address" />
-    ],
-  },
-  argTypes: {
-    children: {
-      table: {
-        disable: true,
-      }
-    },
-  }
+	title: 'Components/TextInput',
+	component: TextInput.Root,
+	args: {
+		children: [
+			<TextInput.Icon>
+				<Envelope />
+			</TextInput.Icon>,
+			<TextInput.Input placeholder="Type your e-mail address" />
+		],
+	},
+	argTypes: {
+		children: {
+			table: {
+				disable: true,
+			}
+		},
+	}
 } as Meta<TextInputRootProps>
 
 export const Default: StoryObj<TextInputRootProps> = {}
 
 export const WithoutIcon: StoryObj<TextInputRootProps> = {
-  args: {
-    children: <TextInput.Input placeholder="Type your e-mail address" />
-  }
+	args: {
+		children: <TextInput.Input placeholder="Type your e-mail address" />
+	}
 }
