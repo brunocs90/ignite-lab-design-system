cdb5e6931a1d13feb32ff73b85265f8724226adb
brunocs90@gmail.com <brunocs90@gmail.com>
2022-10-26 19:23:59 -0300

    alterações e readme

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -3,28 +3,28 @@ import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
 
 export interface HeadingProps {
-	size?: 'sm' | 'md' | 'lg';
-	children: ReactNode;
-	asChild?: boolean;
-	className?: string;
+  size?: 'sm' | 'md' | 'lg';
+  children: ReactNode;
+  asChild?: boolean;
+  className?: string;
 }
 
 export function Heading({ size = 'md', children, asChild, className }: HeadingProps) {
-	const Comp = asChild ? Slot : 'h2';
+  const Comp = asChild ? Slot : 'h2';
 
-	return (
-		<Comp
-			className={clsx(
-				'text-gray-100 font-bold font-sans',
-				{
-					'text-lg': size === 'sm',
-					'text-xl': size === 'md',
-					'text-2xl': size === 'lg',
-				},
-				className
-			)}
-		>
-			{children}
-		</Comp>
-	)
+  return (
+    <Comp 
+      className={clsx(
+        'text-gray-100 font-bold font-sans', 
+        {
+          'text-lg': size === 'sm',
+          'text-xl': size === 'md',
+          'text-2xl': size === 'lg',
+        },
+        className
+      )}
+    >
+      {children}
+    </Comp>
+  )
 }
