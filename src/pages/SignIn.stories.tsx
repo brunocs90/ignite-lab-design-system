1c7a428ad2f315aeb57900db42d195163cde574a
brunocs90@gmail.com <brunocs90@gmail.com>
2022-10-26 22:46:31 -0300

    fixed signin

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -28,8 +28,12 @@ export const Default: StoryObj = {
 	play: async ({ canvasElement }) => {
 		const canvas = within(canvasElement);
 
-		userEvent.type(canvas.getByPlaceholderText("Digite seu e-mail"), "diego@rocketseat.com.br");
+		userEvent.type(
+			canvas.getByPlaceholderText("Digite seu e-mail"),
+			"diego@rocketseat.com.br"
+		);
 		userEvent.type(canvas.getByPlaceholderText("******"), "12345678");
+
 		userEvent.click(canvas.getByRole("button"));
 
 		await waitFor(() => {
